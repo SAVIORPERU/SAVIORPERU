b60bd404a053f2152dfe0fceea4b4148e1f2ead8
Terry <danyel.martel@gmail.com>
2025-05-04 00:13:51 -0500

    data fotosPortada and nosotros created

--- components/hero-footerSection.jsx
+++ components/hero-footerSection.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import styles from './hero-footerSection.module.css'
+import { imagenIzquierda } from '../data/fotosPortada'
 
 const HeroFooterSection = () => {
   return (
     <section className={styles.heroSection}>
       <div className='w-full'>
-        <img
-          src='https://res.cloudinary.com/dkw7q1u6z/image/upload/v1746327259/Photoroom_20250503_213845_eyndsg.jpg'
-          alt='Image 2'
-          className='w-full h-full'
-        />
+        <img src={imagenIzquierda} alt='Image 2' className='w-full h-full' />
       </div>
     </section>
   )
---
+++ data/fotosPortada.js
@@ -0,0 +1,5 @@
+export const imagenIzquierda =
+  'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1746327259/Photoroom_20250503_213845_eyndsg.jpg'
+
+export const imagenDerecha =
+  'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1746327831/foto_2_d31ytj.jpg'
