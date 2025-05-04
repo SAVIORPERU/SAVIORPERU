4fa750daf817d980a76166d3bc54e5ce139c84a5
SAVIORPERU <saviorstore.pe@gmail.com>
2025-05-04 00:17:17 -0500

    Update fotosPortada.js

--- data/fotosPortada.js
+++ data/fotosPortada.js
@@ -2,4 +2,4 @@ export const imagenIzquierda =
   'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1746327259/Photoroom_20250503_213845_eyndsg.jpg'
 
 export const imagenDerecha =
-  'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1746327831/foto_2_d31ytj.jpg'
+  'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1746334846/PORTADA_2_hnriyv.jpg'
