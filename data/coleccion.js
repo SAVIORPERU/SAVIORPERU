a0265b0b19e848abc51015fc6548b575d0398a3c
SAVIORPERU <saviorstore.pe@gmail.com>
2025-05-26 12:11:04 -0500

    cambio de imagen

--- data/coleccion.js
+++ data/coleccion.js
@@ -4,27 +4,27 @@ export const bestSellers = [
     name: 'Ver Pantalones',
     price: 129.99,
     image:
-      'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1745205636/Photoroom_20250420_221506_qw0ozw.webp'
+      'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1748279109/Photoroom_20250526_120124_j2n0su.webp'
   },
   {
     id: 2,
     name: 'Ver Gorros',
     price: 79.99,
     image:
-      'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1745205638/Photoroom_20250420_221724_rnunpi.webp'
+      'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1748279110/Photoroom_20250526_120239_uwajow.webp'
   },
   {
     id: 3,
     name: 'Ver Polos',
     price: 129.99,
     image:
-      'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1745205639/Photoroom_20250420_221836_rg7t8m.webp'
+      'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1748279108/Photoroom_20250526_115459_qrmtfu.webp'
   },
   {
     id: 4,
     name: 'Ver Casacas',
     price: 159.99,
     image:
-      'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1745205638/Photoroom_20250420_221742_llecxs.webp'
+      'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1748279093/Photoroom_20250526_115400_gqcuj6.webp'
   }
 ]
