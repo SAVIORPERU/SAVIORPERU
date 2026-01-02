efd67a367f4c4642012a8bd8d77b0c23bf7a8963
SAVIORPERU <saviorstore.pe@gmail.com>
2026-01-02 12:17:52 -0500

    Update price of Gorro Negro Choosen to 42.5

--- data/Productos.js
+++ data/Productos.js
@@ -120,7 +120,7 @@ export const products = [
     name: 'Gorro Negro Choosen',
     category: 'gorros',
     Estado: 'DISPONIBLE',
-    price: 39.9,
+    price: 42.5,
     image:
       'https://res.cloudinary.com/dkw7q1u6z/image/upload/v1746235207/Gorro_Negro_Choosen_ychlsz.webp',
     image2: ''
