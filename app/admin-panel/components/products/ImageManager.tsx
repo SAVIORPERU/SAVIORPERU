7899f47eb63abd1095fb4012d39ba8e1534c6d19
Terry <danyel.martel@gmail.com>
2026-01-14 01:21:01 -0500

    preset cloudinary fixed

--- app/admin-panel/components/products/ImageManager.tsx
+++ app/admin-panel/components/products/ImageManager.tsx
@@ -48,11 +48,11 @@ const ImageManager: React.FC<ImageManagerProps> = ({
 
     const formData = new FormData()
     formData.append('file', file)
-    formData.append('upload_preset', 'prueba_preset')
+    formData.append('upload_preset', 'saviorperu')
     formData.append('folder', 'ecommerce-products')
 
     const response = await fetch(
-      'https://api.cloudinary.com/v1_1/dniekrmqb/image/upload',
+      'https://api.cloudinary.com/v1_1/saviorperu/image/upload',
       {
         method: 'POST',
         body: formData
